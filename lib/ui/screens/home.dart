bc58ca3c939f32138b3efecb0c2389a7fc6b88db
Le Ngo Duc Anh <54741529+Gernii@users.noreply.github.com>
2022-03-12 16:50:04 +0700

    feat: :sparkles: new search bar

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../widgets/recipe_search.dart';
 import '/model/recipe.dart';
 import '/utils/store.dart';
 import '/ui/widgets/recipe_card.dart';
@@ -18,6 +19,9 @@ class HomeScreen extends StatefulWidget {
 
 class HomeScreenState extends State<HomeScreen> {
   StateModel? appState;
+  final controller = ScrollController();
+  String query = '';
+  String isQueried = '';
 
   DefaultTabController _buildTabView({Widget? body}) {
     const double _iconSize = 20.0;
@@ -29,6 +33,7 @@ class HomeScreenState extends State<HomeScreen> {
           // We set Size equal to passed height (50.0) and infinite width:
           preferredSize: const Size.fromHeight(50.0),
           child: AppBar(
+            backgroundColor: Color(0xFF03ACFE),
             elevation: 2.0,
             bottom: TabBar(
               labelColor: Theme.of(context).indicatorColor,
@@ -63,6 +68,12 @@ class HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  Widget _buildSearch() => RecipeSearch(
+        text: query,
+        hintText: 'Tìm công thức',
+        onChanged: (query) => updateQuery(query),
+      );
+
   Center _buildLoadingIndicator() {
     return const Center(
       child: CircularProgressIndicator(),
@@ -75,7 +86,7 @@ class HomeScreenState extends State<HomeScreen> {
       children: [
         SettingsButton(
           Icons.exit_to_app,
-          "Log out",
+          "Đăng xuất",
           appState!.user!.displayName!,
           () async {
             await StateWidget.of(context).signOutOfGoogle();
@@ -86,49 +97,64 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   TabBarView _buildTabsContent() {
-    Padding _buildRecipes({RecipeType? recipeType, List<String>? ids}) {
+    Scaffold _buildRecipes({RecipeType? recipeType, List<String>? ids}) {
       CollectionReference collectionReference =
           FirebaseFirestore.instance.collection('recipe');
       Stream<QuerySnapshot> stream;
-      // The argument recipeType is set
+      // Kiểm tra loại của công thức
       if (recipeType != null) {
-        stream = collectionReference
-            .where("type", isEqualTo: recipeType.index)
-            .snapshots();
+        if (isQueried != '') {
+          stream = collectionReference
+              .where("type", isEqualTo: recipeType.index)
+              .where("name", isGreaterThanOrEqualTo: isQueried)
+              .snapshots();
+        } else {
+          stream = collectionReference
+              .where("type", isEqualTo: recipeType.index)
+              .snapshots();
+        }
       } else {
-        // Use snapshots of all recipes if recipeType has not been passed
+        // sử dụng snapshots để lưu lại tất cả công thức nếu không truyền loại công thức
         stream = collectionReference.snapshots();
       }
-
       // Define query depeneding on passed args
-      return Padding(
-        // Padding before and after the list view:
-        padding: const EdgeInsets.symmetric(vertical: 5.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder(
-                stream: stream,
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (!snapshot.hasData) return _buildLoadingIndicator();
-                  return ListView(
-                    children: snapshot.data!.docs
-                        // Check if the argument ids contains document ID if ids has been passed:
-                        .where((d) => ids == null || ids.contains(d.id))
-                        .map((document) {
-                      return RecipeCard(
-                        recipe: Recipe.fromMap(
-                            (document.data() as Map<String, dynamic>),
-                            document.id),
-                        onFavoriteButtonPressed: _handleFavoritesListChanged,
-                      );
-                    }).toList(),
-                  );
-                },
+      return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).indicatorColor,
+          onPressed: () => scrollUp(),
+          child: const Icon(Icons.arrow_upward),
+        ),
+        body: Padding(
+          // Padding before and after the list view:
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: Column(
+            children: [
+              _buildSearch(),
+              Expanded(
+                child: StreamBuilder(
+                  stream: stream,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (!snapshot.hasData) return _buildLoadingIndicator();
+                    return ListView(
+                      controller: controller,
+                      children: snapshot.data!.docs
+                          // Check if the argument ids contains document ID if ids has been passed:
+                          .where((d) => ids == null || ids.contains(d.id))
+                          .map((document) {
+                        return RecipeCard(
+                          recipe: Recipe.fromMap(
+                              (document.data() as Map<String, dynamic>),
+                              document.id),
+                          onFavoriteButtonPressed: _handleFavoritesListChanged,
+                        );
+                      }).toList(),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
     }
@@ -154,6 +180,19 @@ class HomeScreenState extends State<HomeScreen> {
           }
         });
       }
+    });
+  }
+
+  void scrollUp() {
+    const double start = 0;
+    // controller.jumpTo(start);
+    controller.animateTo(start,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  void updateQuery(String value) {
+    setState(() {
+      isQueried = value;
     });
   }
 
